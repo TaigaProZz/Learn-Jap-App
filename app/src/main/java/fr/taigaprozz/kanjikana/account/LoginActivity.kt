19b44b1cc1c7d1aee2a9c1de082315680596ab45
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-05-18 15:08:45 +0200

    register activity layout, some verification when registering

--- app/src/main/java/fr/taigaprozz/kanjikana/account/LoginActivity.kt
+++ app/src/main/java/fr/taigaprozz/kanjikana/account/LoginActivity.kt
@@ -1,12 +1,24 @@
 package fr.taigaprozz.kanjikana.account
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import fr.taigaprozz.R
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        val button = findViewById<Button>(R.id.registerButtonLogin)
+        button.setOnClickListener {
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
+++ app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
@@ -2,11 +2,39 @@ package fr.taigaprozz.kanjikana.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import fr.taigaprozz.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val registerEmail = findViewById<EditText>(R.id.emailRegister)
+        val registerPassword = findViewById<EditText>(R.id.passwordRegister)
+        val registerPasswordConfirm = findViewById<EditText>(R.id.passwordRegisterConfirm)
+        val buttonRegister = findViewById<Button>(R.id.registerButton)
+
+        // collect register data from user when register button is pressed
+        buttonRegister.setOnClickListener {
+            var email = registerEmail.text.toString()
+            var password = registerPassword.text.toString()
+            var passwordConfirm = registerPasswordConfirm.text.toString()
+
+        // inspect if user complete correctly
+            if (email.isEmpty()){
+                registerEmail.error = "Un e-mail valide est requis"
+            }
+            if (!email.contains("@")){
+                registerEmail.error = "Un e-mail valide est requis"
+            }
+            if (!email.contains(".")){
+                registerEmail.error = "Un e-mail valide est requis"
+            }
+
+
+
+        }
     }
 }
