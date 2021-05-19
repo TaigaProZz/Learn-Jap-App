a2dcd3dfce69126b961eaabcc934ff749c4eca75
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-05-19 16:04:17 +0200

    login and register sync with firebase
    we can register and it will be saved in firebase DB
    login with a registered account

--- app/src/main/java/fr/taigaprozz/kanjikana/account/LoginActivity.kt
+++ app/src/main/java/fr/taigaprozz/kanjikana/account/LoginActivity.kt
@@ -4,21 +4,58 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import fr.taigaprozz.R
+import fr.taigaprozz.kanjikana.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
 
-        val button = findViewById<Button>(R.id.registerButtonLogin)
-        button.setOnClickListener {
+        val loginEmail = findViewById<EditText>(R.id.email_case_login)
+        val loginPassword = findViewById<EditText>(R.id.password_case_login)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        val gotoRegisterButton = findViewById<Button>(R.id.registerButtonLogin)
+
+        loginButton.setOnClickListener{
+            val email = loginEmail.text.toString()
+            val password = loginPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+
+                if(task.isSuccessful){
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    val user = auth.currentUser
+                    updateUI(user)
+                }
+                else{
+                    Toast.makeText(baseContext, "Connexion impossible",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+            }
+        }
+
+
+        gotoRegisterButton.setOnClickListener {
             val intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+
     }
 }
--- app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
+++ app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
@@ -1,58 +1,68 @@
 package fr.taigaprozz.kanjikana.account
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 import fr.taigaprozz.R
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+
+        val auth = FirebaseAuth.getInstance()
         val registerEmail = findViewById<EditText>(R.id.emailRegister)
         val registerPassword = findViewById<EditText>(R.id.passwordRegister)
         val registerPasswordConfirm = findViewById<EditText>(R.id.passwordRegisterConfirm)
         val buttonRegister = findViewById<Button>(R.id.registerButton)
 
+
         // collect register data from user when register button is pressed
+
         buttonRegister.setOnClickListener {
             val email = registerEmail.text.toString()
             val password = registerPassword.text.toString()
             val passwordConfirm = registerPasswordConfirm.text.toString()
 
-        // inspection of user data for registering
+            // inspection of user data for registering
+
             // email field is empty
-            if (email.isEmpty()){
-                registerEmail.error = "Un e-mail valide est requis" }
-
+            if (email.isEmpty()) {
+                registerEmail.error = "Un e-mail valide est requis"
+            }
             // email field contains " @ " and " . "
-            else if (!email.contains("@")){
+            else if (!email.contains("@")) {
+                registerEmail.error = "Un e-mail valide est requis"
+            } else if (!email.contains(".")) {
                 registerEmail.error = "Un e-mail valide est requis"
             }
-            else if (!email.contains(".")){
-                registerEmail.error = "Un e-mail valide est requis"
-            }
-
             // password field is empty
             else if (passwordConfirm.isEmpty()) {
                 registerPassword.error = "Choisissez votre mot de passe"
             }
             // password and password confirmation are equals
-            else if (passwordConfirm != password){
+            else if (passwordConfirm != password) {
                 registerPasswordConfirm.error = "Le mot de passe ne correspond pas"
             }
             // password is a minimum strong
-            else if (passwordConfirm.length < 7){
+            else if (passwordConfirm.length < 7) {
                 registerPassword.error = "Le mot de passe est trop petit"
             }
-            else if(password.contentEquals(password.uppercase())){
-                println("maj")
 
-        }
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+                .addOnFailureListener(this){
 
-
+                }
 
         }
     }
