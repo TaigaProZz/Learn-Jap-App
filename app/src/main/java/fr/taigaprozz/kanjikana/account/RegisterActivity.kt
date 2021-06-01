58a6ac095831de82c110005f069c163bd667f3bf
TaigaProZz <74510014+TaigaProZz@users.noreply.github.com>
2021-06-01 15:33:55 +0200

    signin google done

--- app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
+++ app/src/main/java/fr/taigaprozz/kanjikana/account/RegisterActivity.kt
@@ -3,14 +3,13 @@ package fr.taigaprozz.kanjikana.account
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.text.Html
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
@@ -18,7 +17,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import fr.taigaprozz.R
-import fr.taigaprozz.databinding.ActivityRegisterBinding.inflate
 
 
 class RegisterActivity : AppCompatActivity() {
@@ -26,102 +24,104 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var mGoogle: GoogleSignInClient
     private var RC_SIGN_IN = 123
 
+
     override fun onStart() {
         super.onStart()
 
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
+        val user = auth.currentUser
+        if (user != null) {
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_register)
 
         auth = FirebaseAuth.getInstance()
-
 
         // call request function to google and firebase
         createRequest()
 
-        // google sign in button
-        binding.googleRegisterButton.setOnClickListener {
+        val registerGoogleButton = findViewById<Button>(R.id.googleRegisterButton)
+        registerGoogleButton.setOnClickListener {
+            println("test")
             signIn()
         }
 
 
-
-        // arrow to go back function
-        backArrow()
-
         // collect register data from user when register button is pressed
-            val auth = FirebaseAuth.getInstance()
-            val registerEmail = findViewById<EditText>(R.id.emailRegister)
-            val registerPassword = findViewById<EditText>(R.id.passwordRegister)
-            val registerPasswordConfirm = findViewById<EditText>(R.id.passwordRegisterConfirm)
+        val auth = FirebaseAuth.getInstance()
+        val registerEmail = findViewById<EditText>(R.id.emailRegister)
+        val registerPassword = findViewById<EditText>(R.id.passwordRegister)
+        val registerPasswordConfirm = findViewById<EditText>(R.id.passwordRegisterConfirm)
 
-            val registerButton = findViewById<Button>(R.id.registerButton).setOnClickListener {
-                var validationCheck = true
-                val email = registerEmail.text.toString()
-                val password = registerPassword.text.toString()
-                val passwordConfirm = registerPasswordConfirm.text.toString()
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            println("test")
+            var validationCheck = true
+            val email = registerEmail.text.toString()
+            val password = registerPassword.text.toString()
+            val passwordConfirm = registerPasswordConfirm.text.toString()
 
-                var upperBool = false
-                for (c in password) {
-                    if (c.isUpperCase()) {
-                        upperBool = true
-                    }
+            var upperBool = false
+            for (c in password) {
+                if (c.isUpperCase()) {
+                    upperBool = true
                 }
+            }
 
-                // inspection of user data for registering
+            // inspection of user data for registering
 
-                // email field is empty
-                if (email.isEmpty()) {
-                    registerEmail.error = "Un e-mail est requis"
-                    validationCheck = false
-                }
+            // email field is empty
+            if (email.isEmpty()) {
+                registerEmail.error = "Un e-mail est requis"
+                validationCheck = false
+            }
 
-                // email field contains " @ " and " . "
-                else if (!email.contains("@")) {
+            // email field contains " @ " and " . "
+            else if (!email.contains("@")) {
 
-                    registerEmail.error = "Un e-mail valide est requis"
-                    validationCheck = false
-                } else if (!email.contains(".")) {
-                    registerEmail.error = "Un e-mail valide est requis"
-                    validationCheck = false
-                }
+                registerEmail.error = "Un e-mail valide est requis"
+                validationCheck = false
+            } else if (!email.contains(".")) {
+                registerEmail.error = "Un e-mail valide est requis"
+                validationCheck = false
+            }
 
-                // password field is empty
-                else if (passwordConfirm.isEmpty()) {
-                    registerPassword.error = "Choisissez votre mot de passe"
-                    validationCheck = false
-                }
+            // password field is empty
+            else if (passwordConfirm.isEmpty()) {
+                registerPassword.error = "Choisissez votre mot de passe"
+                validationCheck = false
+            }
 
-                // password is a minimum strong
-                else if (passwordConfirm.length < 7) {
-                    registerPassword.error = "Le mot de passe est trop petit"
-                    validationCheck = false
-                } else if (!upperBool) {
-                    registerPassword.error = "Il faut une majuscule minimum"
-                    validationCheck = false
-                }
+            // password is a minimum strong
+            else if (passwordConfirm.length < 7) {
+                registerPassword.error = "Le mot de passe est trop petit"
+                validationCheck = false
+            } else if (!upperBool) {
+                registerPassword.error = "Il faut une majuscule minimum"
+                validationCheck = false
+            }
 
-                // password and password confirmation are equals
-                else if (passwordConfirm != password) {
-                    registerPasswordConfirm.error = "Le mot de passe ne correspond pas"
-                    validationCheck = false
-                }
+            // password and password confirmation are equals
+            else if (passwordConfirm != password) {
+                registerPasswordConfirm.error = "Le mot de passe ne correspond pas"
+                validationCheck = false
+            }
 
-                if (validationCheck) {
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
-                        val intent = Intent(applicationContext, LoginActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
+            if (validationCheck) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                    val intent = Intent(applicationContext, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
                 }
             }
         }
+
+        // arrow to go back function
+        backArrow()
+    }
 
     //
     private fun createRequest() {
@@ -129,26 +129,28 @@ class RegisterActivity : AppCompatActivity() {
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-          mGoogle = GoogleSignIn.getClient(this, gso)
+        mGoogle = GoogleSignIn.getClient(this, gso)
     }
 
+    // start google sign in activity
     private fun signIn() {
-            val intent = mGoogle.signInIntent
-            startActivityForResult(intent, RC_SIGN_IN)
+
+        val intent = mGoogle.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+
     }
 
-    override fun onActivityResult(requestCode: Int ,resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode== RC_SIGN_IN) {
+        if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
 
             try {
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
-            }
-            catch (e: ApiException){
+            } catch (e: ApiException) {
                 Toast.makeText(this, "Impossible de se connecter", Toast.LENGTH_SHORT).show()
             }
         }
@@ -170,19 +172,14 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-
     private fun updateUI(user: FirebaseUser?) {
     }
 
 
-
-
     // back arrow
     private fun backArrow() {
-        val binding = inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        binding.backArrow.setOnClickListener {
+        val backArrow = findViewById<ImageView>(R.id.backArrow)
+        backArrow.setOnClickListener {
             finish()
             startActivity(Intent(applicationContext, FirstActivity::class.java))
         }
